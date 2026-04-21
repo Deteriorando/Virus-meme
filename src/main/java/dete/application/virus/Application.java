0481a759e254f0fbc3114c4be1ca16b3c731d003
Deteriorando <arthurgamer9999yt@gmail.com>
2026-04-20 22:02:40 -0300

    Rename Presente

--- src/main/java/dete/application/virus/Application.java
+++ src/main/java/dete/application/virus/Application.java
@@ -16,7 +16,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Virus test");
+        stage.setTitle("Presente");
         stage.setScene(scene);
 
         stage.setOnCloseRequest(windowEvent -> {
